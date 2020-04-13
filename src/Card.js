718b309b223eca7b9376c2abacfcb81a7a287729
Vinayak Gaikwad <gaikwadvinayak291@gmail.com>
2020-04-13 14:22:50 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+
+		<div>
+			<img alt = 'robot_photo' src='https://robohash.org/vinayak' />
+			<div>
+				<h2>Vinayak</h2>
+				<p>nayak@gmail.com</p>
+			</div>
+
+		</div>
+		
+
+		
+		
+	);
+}
+
+export default Card;
